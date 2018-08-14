b0905b3ad296fb60a7bbe01e36f2a3d4c70a0d92
Deepayan Bhadra <dbhadra@umd.edu>
2018-08-14 15:32:13 -0400

    Routine file update

--- Anderson_Brockett_estimates.m
+++ Anderson_Brockett_estimates.m
@@ -1,6 +1,8 @@
+% Compares the Anderson and Brockett convergence estimates for a 3-agent topology. Refer pages 5-6 in the PDF
+
 clc;clear;
 D = [-1 0 1;0 1 -1;1 -1 0];
-Le = D'*D;
+Le = D'*D; % Edge Laplacian matrix
 u1 = 0:1:100;
 k = 1;
 Le_T = Le(1:2,1:2);
@@ -8,8 +10,9 @@ Le_T = Le(1:2,1:2);
 u2 = 100;
 eig_Le_T = eig(Le_T);
 p = 2;
-eta_sq = 2*k*min(eig_Le_T)*u1./((1+sqrt(p)*norm(D)*u2)^2);
+eta_sq = 2*k*min(eig_Le_T)*u1./((1+sqrt(p)*norm(D)*u2)^2); % Temporary parameter
 
+% See eqn.(25) in PDF for below
 a = ((p*u2)^1.5*max(eig_Le_T))./(1+2*u1*min(eig_Le_T));
 b = 2*u1*min(eig_Le_T)./(max(eig_Le_T)*(1+2*u1*min(eig_Le_T)));
 o = -a+sqrt(a.^2+b);
@@ -19,8 +22,9 @@ J = (1-o.^2/max(eig(inv(D))))./(1-eta_sq);
 plot(u1,J)
 axis([0 100 1 1.02])
 hold on;
-
-% Correct plot
 xlabel('u1') % x-axis label
 ylabel('J(p,u1,u2)') % y-axis label
+hold off;
+
+% EOF
 
