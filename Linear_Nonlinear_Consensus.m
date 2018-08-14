bce2b5fc433c4147fd7ab7484f80d09f3dc8a74a
Deepayan Bhadra <dbhadra@umd.edu>
2018-08-14 12:40:11 -0400

    Routine file cleaning

--- Linear_Nonlinear_Consensus.m
+++ Linear_Nonlinear_Consensus.m
@@ -1,34 +1,42 @@
 clc;clear;
-k = 1;
-D = [1 0 0 0 1;-1 1 0 -1 0;0 -1 1 0 -1;0 0 -1 1 0];
+
+%% Consensus with Linear Protocols
+
+k = 1; % control gain
+D = [1 0 0 0 1;...
+    -1 1 0 -1 0;...
+    0 -1 1 0 -1;...
+    0 0 -1 1 0]; % incidence matrix
 x0 = [0.1 0.1 0.77 0.8]';
 y0 = [0.8 0.1 0.78 0]';
 t = 0:0.1:5;
-% g1 = (t.^2+1).*(t.^2+1).*(sin(t).^2);
 L = zeros(4,4,length(t));
 LapPotx = zeros(length(t),1);
 LapPoty = zeros(length(t),1);
+% Laplacian potential as a measure of total disagreement among the nodes
 
-
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
-
-[t,x] = ode45(@(t,x) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t.^2+1)^2*(sin(5*t)^2)])*D'*x,t, x0); 
-[t,y] = ode45(@(t,y) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'*y,t, y0); 
+[t,x] = ode45(@(t,x) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2)... 
+                (t.^2+1)^2*(sin(5*t)^2)])*D'*x,t, x0); 
+            
+[t,y] = ode45(@(t,y) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2)...
+                (t^2+1)^2*(sin(5*t)^2)])*D'*y,t, y0); 
+            
+% (x,y) State trajectory of the 4 agents in the network. Refer (9) in PDF          
 
 T=0:0.1:5;
-    for i =1:length(T),
-      t=T(i);
+for i =1:length(T),
+t=T(i);
 L(:,:,i) = D*diag([(t.^2+1).*(t.^2+1).*(sin(t).^2),...
-                   (t.^2+1).*(t.^2+1).*(sin(2*t).^2),...
-                   (t.^2+1).*(t.^2+1).*(sin(3*t).^2),...
-                   (t.^2+1).*(t.^2+1).*(sin(4*t).^2),... 
-                   (t.^2+1).*(t.^2+1).*(sin(5*t).^2)])*D';
-               LapPotx(i,1) = x(i,:)*L(:,:,i)*x(i,:)';
-                              LapPoty(i,1) = y(i,:)*L(:,:,i)*y(i,:)';
+                (t.^2+1).*(t.^2+1).*(sin(2*t).^2),...
+                (t.^2+1).*(t.^2+1).*(sin(3*t).^2),...
+                (t.^2+1).*(t.^2+1).*(sin(4*t).^2),...
+                (t.^2+1).*(t.^2+1).*(sin(5*t).^2)])*D';
+            
+LapPotx(i,1) = x(i,:)*L(:,:,i)*x(i,:)';
+LapPoty(i,1) = y(i,:)*L(:,:,i)*y(i,:)';
 
-    end
+end
+% L is the Laplacian Matrix
    
     
 subplot(2,1,1)
@@ -52,21 +60,10 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3','Agent 4')
 
+pause(15)
 
-
-% % Spanning tree edges
-% 
-xe = D'*x';
-xe = xe';
-xT = xe(:,1:3);
-norm_xT = zeros(1,length(t));
-for i =1:length(t)
-    norm_xT(i) = norm(xe(i,:));
-end
- plot(t,norm_xT') % Possible error in plot
-
-% For n = 3 and triangle topology 
-
+%% For n = 3 and triangle topology (n : no. of agents)
+clc;clear;
 k = 1;
 D = [-1 0 1;1 -1 0;0 1 -1];
 x0 = [0.1 0.5 1]';
@@ -75,24 +72,16 @@ t = 0:0.1:5;
 LapPotx = zeros(length(t),1);
 LapPoty = zeros(length(t),1);
 
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
-
 [t,x] = ode45(@(t,x) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'*x,t, x0); 
 [t,y] = ode45(@(t,y) -k*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'*y,t, y0); 
 
 T=0:0.1:5;
-    for i =1:length(T),
-      t=T(i);
-L(:,:,i) = D*diag([(t.^2+1).*(t.^2+1).*(sin(t).^2),...
-                   (t.^2+1).*(t.^2+1).*(sin(2*t).^2),...
-                   (t.^2+1).*(t.^2+1).*(sin(3*t).^2)
-              ])*D';
-               LapPotx(i,1) = x(i,:)*L(:,:,i)*x(i,:)';
-                              LapPoty(i,1) = y(i,:)*L(:,:,i)*y(i,:)';
-
-    end
+for i =1:length(T),
+t=T(i);
+L(:,:,i) = D*diag([(t.^2+1).*(t.^2+1).*(sin(t).^2),(t.^2+1).*(t.^2+1).*(sin(2*t).^2),(t.^2+1).*(t.^2+1).*(sin(3*t).^2)])*D';
+LapPotx(i,1) = x(i,:)*L(:,:,i)*x(i,:)';
+LapPoty(i,1) = y(i,:)*L(:,:,i)*y(i,:)';
+end
    
     
 subplot(2,1,1)
@@ -114,27 +103,34 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3')
 
-% For n = 6 and ~hexagon topology
+pause(15)
 
+%% For n = 6 and hexagon topology
+clc;clear;
 
 k = 1;
-D = [-1 0 0 0 -1 0 0 0 1;1 -1 0 0 0 0 0 0 -1;0 0 0 1 1 -1 0 0 0;0 1 -1 0 0 0 0 1 0;0 0 1 -1 0 1 -1 0 0;0 0 0 0 0 0 1 -1 0];
+D = [-1 0 0 0 -1 0 0 0 1;...
+    1 -1 0 0 0 0 0 0 -1;...
+    0 0 0 1 1 -1 0 0 0;...
+    0 1 -1 0 0 0 0 1 0;...
+    0 0 1 -1 0 1 -1 0 0;...
+    0 0 0 0 0 0 1 -1 0];
 x0 = [0.1 0.1 1 2.5 0 2]';
 y0 = [1.5 0.9 2 0 3 1]';
-t = 0:0.1:5; % Start diverging with higher t? 
+t = 0:0.1:5; 
 
 LapPotx = zeros(length(t),1);
 LapPoty = zeros(length(t),1);
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
 
-[t,x] = ode45(@(t,x) -D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'*x,t, x0); 
-[t,y] = ode45(@(t,y) -D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'*y,t, y0); 
+[t,x] = ode45(@(t,x) -D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2)...
+                (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'*x,t, x0); 
+            
+[t,y] = ode45(@(t,y) -D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2)...
+                (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'*y,t, y0); 
 
 T=0:0.1:5;
-    for i =1:length(T),
-      t=T(i);
+for i =1:length(T),
+t=T(i);
 L(:,:,i) = D*diag([(t.^2+1).*(t.^2+1).*(sin(t).^2),...
                    (t.^2+1).*(t.^2+1).*(sin(2*t).^2),...
                    (t.^2+1).*(t.^2+1).*(sin(3*t).^2),...
@@ -147,7 +143,7 @@ L(:,:,i) = D*diag([(t.^2+1).*(t.^2+1).*(sin(t).^2),...
                LapPotx(i,1) = x(i,:)*L(:,:,i)*x(i,:)';
                               LapPoty(i,1) = y(i,:)*L(:,:,i)*y(i,:)';
 
-    end
+end
    
     
 subplot(2,1,1)
@@ -176,7 +172,10 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3','Agent 4','Agent 5','Agent 6')
 
-% For n = 3, triangle topology and double integrator dynamics 
+pause(15)
+
+%% For n = 3, triangle topology and double integrator dynamics 
+clc;clear;
 
 k = 1;
 n = 3;
@@ -190,13 +189,14 @@ w0 = [y0;y0_dot];
 t = 0:0.1:20;
 g=1;
 
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
+[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'...
+                -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'], 1)*z,t,z0); 
+            
+[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'...
+                -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'], 1)*w,t,w0); 
 
-[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'], 1)*z,t,z0); 
-[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)])*D'], 1)*w,t,w0); 
-
+% kron : Kronecker delta function            
+            
 x = z(:,1:3);
 y = w(:,1:3);
 figure(1)
@@ -209,8 +209,11 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3')
 
-% For n = 4, square topology and double integrator dynamics 
+pause(15)
+
+%% For n = 4, square topology and double integrator dynamics 
 clc;clear;
+
 k = 1;
 n = 4;
 D = [1 0 0 0 1;-1 1 0 -1 0;0 -1 1 0 -1;0 0 -1 1 0];
@@ -222,8 +225,13 @@ z0 = [x0;x0_dot];
 w0 = [y0;y0_dot];
 t = 0:0.1:20;
 g = 1;
-[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'], 1)*z,t,z0); 
-[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'], 1)*w,t,w0); 
+
+[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'...
+                -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'], 1)*z,t,z0); 
+            
+[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'...
+                -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2)])*D'], 1)*w,t,w0);
+            
 x = z(:,1:4);
 y = w(:,1:4);
 figure(1)
@@ -238,23 +246,10 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3','Agent 4')
 
-% Simple communication delay case
-s = tf('s')
-sys = exp(-0.1*s);    
-sysx = pade(sys,3);
-x0 = randn(10,1);
-D = diag([2 3 4 4 4 4 4 4 3 2]);
-A = [0 1 1 0 0 0 0 0 0 0;1 0 1 1 0 0 0 0 0 0;1 1 0 1 1 0 0 0 0 0;0 1 1 0 1 1 0 0 0 0;0 0 1 1 0 1 1 0 0 0;0 0 0 1 1 0 1 1 0 0;0 0 0 0 1 1 0 1 1 0;0 0 0 0 0 1 1 0 1 1;0 0 0 0 0 0 1 1 0 1;0 0 0 0 0 0 0 1 1 0];
-L = D-A;
-sol = dde23(@ddefun,[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1],x0,[0 5]);
-H = 1/(s+L*sysx);
-K = H*x0;
-[y,t] = impulse(K)
+pause(15)
 
-
-sol = dde23('ddefun',[1 1 1 1 1 1 1 1 1 1],[0 0 0 0 0 0 0 0 0 0],[0, 100]);
-
-% Non-linear protocol case
+%% Consensus with nonlinear protocol
+clc;clear;
 
 x0 = [0.1 0.5 1]';
 y0 = [1.5 0.9 2]';
@@ -282,22 +277,34 @@ xlabel('time','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3')
 
-% For n = 6 and ~hexagon topology, Double Integrator case
+pause(15)
+
+%% For n = 6 and ~hexagon topology, Double Integrator case
+clc;clear;
 
 k = 1;
 n = 6;
-D = [-1 0 0 0 -1 0 0 0 1;1 -1 0 0 0 0 0 0 -1;0 0 0 1 1 -1 0 0 0;0 1 -1 0 0 0 0 1 0;0 0 1 -1 0 1 -1 0 0;0 0 0 0 0 0 1 -1 0];
+D = [-1 0 0 0 -1 0 0 0 1;...
+    1 -1 0 0 0 0 0 0 -1;...
+    0 0 0 1 1 -1 0 0 0;...
+    0 1 -1 0 0 0 0 1 0;...
+    0 0 1 -1 0 1 -1 0 0;...
+    0 0 0 0 0 0 1 -1 0];
 x0 = [0.1 0.1 1 2.5 0 2]';
 x0_dot = [0 0 0 0 0 0]';
 y0 = [1.5 0.9 2 0 3 1]';
 y0_dot = [0 0 0 0 0 0]';
 z0 = [x0;x0_dot];
 w0 = [y0;y0_dot];
-t = 0:0.1:20; % Start diverging with higher t? 
+t = 0:0.1:20; 
 
 g = 1;
-[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'], 1)*z,t,z0); 
-[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'], 1)*w,t,w0);
+[t,z] = ode45(@(t,z) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)...
+                (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'], 1)*z,t,z0); 
+
+[t,w] = ode45(@(t,w) kron([zeros(n,n) eye(n);-D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2)...
+                (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D' -g*D*diag([(t^2+1)^2*(sin(t)^2) (t^2+1)^2*(sin(2*t)^2) (t^2+1)^2*(sin(3*t)^2) (t^2+1)^2*(sin(4*t)^2) (t^2+1)^2*(sin(5*t)^2) (t^2+1)^2*(sin(6*t)^2) (t^2+1)^2*(sin(7*t)^2) (t^2+1)^2*(sin(8*t)^2) (t^2+1)^2*(sin(9*t)^2)])*D'], 1)*w,t,w0);
+
 figure(1)
 plot(x(:,1),y(:,1),'c')
 hold on;
@@ -314,7 +321,10 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3','Agent 4','Agent 5','Agent 6')
 
-% For n = 3 and triangle topology, constant weights
+pause(15)
+
+%% For n = 3 and triangle topology, constant weights
+clc;clear;
 
 k = 1;
 D = [-1 0 1;1 -1 0;0 1 -1];
@@ -322,10 +332,6 @@ x0 = [0.1 0.1 1]';
 y0 = [1.5 0.9 2]';
 t = 0:0.1:20;
 W = eye(3);
-
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
 
 [t,x] = ode45(@(t,x) -D*W*D'*x,t, x0); 
 [t,y] = ode45(@(t,y) -D*W*D'*y,t, y0); 
@@ -339,19 +345,17 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3')
 
-% For n = 4 and square topology, constant weights
+pause(15)
 
+%% For n = 4 and square topology, constant weights
 clc;clear;
+
 k = 1;
 D = [1 0 0 0 1;-1 1 0 -1 0;0 -1 1 0 -1;0 0 -1 1 0];
 x0 = [0.1 0.1 0.77 0.78]';
 y0 = [1.5 0.9 1.78 0]';
 t = 0:0.1:20;
 W = eye(5);
-
-% for i=1:5
-% g_sq(i,:) = (t.^2+1).*(t.^2+1).*(sin(i*t).^2);
-% end
 
 [t,x] = ode45(@(t,x) -k*D*W*D'*x,t, x0); 
 [t,y] = ode45(@(t,y) -k*D*W*D'*y,t, y0); 
@@ -367,8 +371,11 @@ xlabel('x-coordinates','fontweight','bold','fontsize',16);
 ylabel('y-coordinates','fontweight','bold','fontsize',16);
 legend('Agent 1','Agent 2','Agent 3','Agent 4')
 
-% Lyapunov analysis of spanning tree
+pause(15)
+
+%% Lyapunov analysis of spanning tree
 clc;clear;
+
 k = 1;
 DT = [1 0 0;1 1 0;0 1 1;0 0 1];
 x0 = [0.1 0.5 0.9]';
@@ -383,7 +390,7 @@ for i =1:length(t)
    eigV(i,:) = eig(v+v'); 
 end
 
-
+% EOF
 
 
 
